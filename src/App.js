48a3013f963102839aa169f7337d94a9ca1e6816
Sol Battaglia <solbattaglia@gmail.com>
2020-07-30 16:31:31 -0300

    Cambio2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sol Battaglia <code>src/App.js</code> and save to reload.
+          Sol Battaglia <code>src/App.js</code>.
         </p>
         <a
           className="App-link"
